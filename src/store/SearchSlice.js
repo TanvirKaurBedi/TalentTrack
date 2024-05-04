6725f6872b528351953c204d896ec811aceb135f
tanvir.kour <tanvir.kour@codenicely.in>
2024-05-04 13:47:32 +0530

    added a  slice file for search

---
+++ src/store/SearchSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    value : 0
+}
+export const searchSlice = createSlice({
+    name:"searchSlice",
+    initialState,
+    reducers:{
+
+    }
+})
+// Action Creators
+export const {} = searchSlice.actions
+export default  searchSlice.reducer
